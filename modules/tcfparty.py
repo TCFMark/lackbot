f521d46d91f08b94c78deccd80345c269d2d24fa
TCFMark <markstevenhearn@gmail.com>
2014-04-17 13:03:48 +0100

    Reimplemented string length limit.
    This is to avoid HTTP Error 414.

--- modules/tcfparty.py
+++ modules/tcfparty.py
@@ -2,19 +2,34 @@
 # -*- coding: utf-8 -*-
 '''TCFParty: Unofficial Translation Party API
 '''
-from goslate import Goslate
-gs = Goslate()
+
+import translate
 
 def tcfparty(en1):
 	en2 = None
 	en0 = None
 	count = 0
+	en1 = en1.encode('utf-8')
+
 	for x in range(0, 32):
 		count += 1
-		en1 = limitLen(en1, 1600)
-		ja = gs.translate(en1, 'ja', 'en')
-		ja = limitLen(ja, 1600)
-		en2 = gs.translate(ja, 'en', 'ja')
+
+		# Translate to Japanese
+		en1 = limitLen(en1, 400)
+		try:
+			ja, _lang = translate.translate(en1, 'en', 'ja')
+		except ValueError:
+			break
+		ja = ja.encode('utf-8')
+
+		# Translate back to English
+		ja = limitLen(ja, 400)
+		try:
+			en2, _lang = translate.translate(ja, 'ja', 'en')
+		except ValueError:
+			break
+		en2 = en2.encode('utf-8')
+
 		if en1 == en2:
 			break
 		if en2 == en0:
