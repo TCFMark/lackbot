9e7990edfe9b3ff3b6ded6b811d50d71053ca52b
TCFMark <markstevenhearn@gmail.com>
2014-05-04 13:02:35 +0100

    Added logging to peech for #28.
    Remaining: clock, codepoints, etymology, head, info, oblique,
    ping, quip, reload, remind, search, seen, tell, validate, vincent,
    weather, wikipedia, wiktionary

--- modules/peech.py
+++ modules/peech.py
@@ -2,12 +2,13 @@
 peech.py - Phenny 'tupid 'peech Correction Module
 """
 
-import re, sys
+import re, sys, logging
 
 def correct(phenny, input):
    matches = re.findall(r'(?:^|\s)s+[hz]?((?=(?=c[^i^e])|[bdfgjklmnpqrtvwx]).*?)(?=$|\s)', input.group(), re.IGNORECASE)
    for match in matches:
       if (not "phinxchild" in match) and (input.nick != "prcjac"):
-            phenny.reply("'" + match)
+         logging.debug(__name__ + ': ' + input.nick + ' said s' + match + ' when they obviously meant \'' + match)
+         phenny.reply("'" + match)
 correct.rule = r'.*?(^|\s)[Ss]+[HZhz]?[bcdfgjklmnpqrtvwxBCDFGJKLMNPQRTVWX].*'
 
