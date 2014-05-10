4dbfa4d90a26069bf5c0bf713a357389e7600464
TCFMark <markstevenhearn@gmail.com>
2014-05-10 08:52:56 +0100

    Added logging to quip for #28
    Remaining: reload, remind, search, seen, tell, validate, weather,
    wiktionary

--- modules/quip.py
+++ modules/quip.py
@@ -5,6 +5,7 @@ quip.py - Phenny patel pyquips module
 import sys, pyquips, cgi, os
 
 def quip(phenny, input):
+   logging.debug('Adding quip: ' + input.group(2))
    pyquips.run(input.group(2) + "\n")
    phenny.reply("Quip added! Check it out at http://tcfmark.dnsd.info/quips/")
 quip.commands = ['quip']
