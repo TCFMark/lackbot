959b59be1ea9ed4c0ee3b0f00fa3c1a304c37284
TCFMark <markstevenhearn@gmail.com>
2014-05-10 08:50:44 +0100

    Added possibly superfluous logging to ping for #28
    Remaining: quip, reload, remind, search, seen, tell, validate, weather,
    wiktionary

--- modules/ping.py
+++ modules/ping.py
@@ -29,30 +29,36 @@ def getWordList(minwords):
    return words
 
 def hello(phenny, input): 
+   logging.debug('Saying hello to ' + input.nick)
    greeting = random.choice(('Hi', 'Hey', 'Hello'))
    punctuation = random.choice(('', '!', ' :)', ' <3'))
    phenny.say(greeting + ' ' + input.nick + punctuation)
 hello.rule = r'(?i)(hi|hello|hey) $nickname[ \t]*$'
 
 def interjection(phenny, input): 
+   logging.debug('Yelling back at ' + input.nick)
    phenny.say(input.nick + '!')
 interjection.rule = r'$nickname!'
 interjection.priority = 'high'
 interjection.thread = False
 
 def idiot(phenny, input):
+   logging.debug(input.nick + ' called me an idiot :(')
    phenny.say(':(')
 idiot.rule = r'$nickname: idiot'
 
 def thanks(phenny, input):
+   logging.debug(input.nick + ' said thanks!')
    phenny.say(input.nick + ': welcome!')
 thanks.rule = r'thanks(,)? $nickname[ \t]*$'
 
 def wtf(phenny, input):
+   logging.debug('WTF indeed, ' + input.nick)
    phenny.reply("right!?")
 wtf.rule = r'(?i)wtf.*'
 
 def wow(phenny, input):
+   logging.debug('Wow, so ' + input.nick + '. Activating doge mode')
    try:         
       phenny.say(random.choice(["so ", "such "]) + getWordList(1)[1])
       phenny.say(random.choice(["many ", "how "]) + getWordList(1)[1])
@@ -64,18 +70,22 @@ def wow(phenny, input):
 wow.rule = r'(?i)wow$'
 
 def nsfw(phenny, input):
+   logging.debug('Assuring SFWness to ' + input.nick)
    phenny.say("it totally is safe for work")
 nsfw.rule = r'(?i).*?nsfw.*'
 
 def rediculous(phenny, input):
+   logging.debug(input.nick + 'said rediculous :(')
    phenny.reply("*ridiculous")
 rediculous.rule = r'(?i).*?rediculous.*'
 
 def lol(phenny, input):
+   logging.debug('Laughter detected! LOL!')
    phenny.say(makelaugh())
 lol.rule = r'(?i)lol.*'
 
 def haha(phenny, input):
+   logging.debug('Laughter detected! HAHA!')
    phenny.say(makelaugh())
 haha.rule = r'(?i)hah.*'
 
@@ -99,14 +109,17 @@ def makehaha():
    return hahaha
 
 def work(phenny, input):
+   logging.debug('Work buzzer going off')
    phenny.reply('BZZZZZZZT')
 work.rule = r'(?i).*?(cfl|corefiling|(\s|^)wiki(\s|$)|(\s|^)(s|\')tory(\s|$)|seahorse|donkey|romp|tnfc|tnwsp|magnify|spidermonkey|smmf|plega|djb|xiif|xbrl|(\s|^)sys(\s|$)|synerg).*'
+
+def gross(phenny, input):
+   logging.debug('Gross')
+    response = random.choice(['icky', 'yuck', 'gross', 'filfy', 'beastly'])
+    phenny.say(response)
+gross.rule = r'(?i).*?gross.*'
 
 if __name__ == '__main__': 
    print __doc__.strip()
 
-def gross(phenny, input):
-    response = random.choice(['icky', 'yuck', 'gross', 'filfy', 'beastly'])
-    phenny.say(response)
-gross.rule = r'(?i).*?gross.*'
 
