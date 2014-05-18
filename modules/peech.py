ae21ad2a067bc2aff2cb20f97e4067d678c4d6c8
Ben Holmes <bdvholmes@gmail.com>
2014-05-18 18:54:17 +0100

    Swashbuckling is not washbuckling

--- modules/peech.py
+++ modules/peech.py
@@ -5,10 +5,10 @@ peech.py - Phenny 'tupid 'peech Correction Module
 import re, sys, logging
 
 def correct(phenny, input):
-   matches = re.findall(r'(?:^|\s)s+[hz]?((?=(?=c[^i^e])|[bdfgjklmnpqrtvwx]).*?)(?=$|\s)', input.group(), re.IGNORECASE)
+   matches = re.findall(r'(?:^|\s)s+[hz]?((?=(?=c[^i^e])|[bdfgjklmnpqrtvx]).*?)(?=$|\s)', input.group(), re.IGNORECASE)
    for match in matches:
       if (not "phinxchild" in match) and (input.nick != "prcjac"):
          logging.debug(input.nick + ' said s' + match + ' when they obviously meant \'' + match)
          phenny.reply("'" + match)
-correct.rule = r'.*?(^|\s)[Ss]+[HZhz]?[bcdfgjklmnpqrtvwxBCDFGJKLMNPQRTVWX].*'
+correct.rule = r'.*?(^|\s)[Ss]+[HZhz]?[bcdfgjklmnpqrtvxBCDFGJKLMNPQRTVWX].*'
 
